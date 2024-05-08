44e89ac6ad90d7b62f6a47d302f5df8a96858283
Charlie Wang <charliewang436@uchicago.edu>
2024-05-08 15:46:33 -0500

    final

--- pa5.py
+++ pa5.py
@@ -4,15 +4,17 @@
 this is the code for pa5
 '''
 
+
 def gcd(num1, num2):
     '''finds the greatest common denominator of two numbers'''
-    if num2>num1:
+    if num2 > num1:
         temp = num1
         num1 = num2
         num2 = temp
     if num2 == 0:
         return num1
-    return gcd(num2, num1%num2)
+    return gcd(num2, num1 % num2)
+
 
 def remove_pairs(string):
     '''removes repetitive pairs in a maze path'''
@@ -23,6 +25,7 @@ def remove_pairs(string):
         return remove_pairs(string[2:])
     return string[0] + remove_pairs(string[1:])
 
+
 def sign(number):
     '''helper function for bisection_root'''
     if number > 0:
@@ -30,13 +33,12 @@ def sign(number):
     if number < 0:
         return "neg"
 
+
 def bisection_root(function, x1, x2):
     '''finds the x intercept of a function given two guesses'''
-    print(function(x1))
-    print(function(x2))
-    if function(x1) > 0  and function(x2) > 0:
+    if function(x1) > 0 and function(x2) > 0:
         raise ValueError()
-    if function(x1) < 0  and function(x2) < 0:
+    if function(x1) < 0 and function(x2) < 0:
         raise ValueError()
     if function(x1) <= 0.0000001 and function(x1) >= -0.0000001:
         return x1
